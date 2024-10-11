07777b844c008140c4d5fcc0cde04676e9aa9d44
Pramod Koujalagi <69685221+pramodkoujalagi@users.noreply.github.com>
2024-10-11 09:20:15 +0530

    Cleaned n optimized for efficiency

--- value.py
+++ value.py
@@ -1,18 +1,14 @@
 import math
-
-
 class Value:
+    """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, _children=(), _op='', label=''):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
-        self.grad = 0.0
+        self.grad = 0
+        # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op
-        self.label = label
-
-    def __repr__(self):
-        return f"Value(data={self.data})"
+        self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -24,9 +20,6 @@ class Value:
         out._backward = _backward
 
         return out
-
-    def __radd__(self, other):
-        return self + other
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -40,8 +33,7 @@ class Value:
         return out
 
     def __pow__(self, other):
-        assert isinstance(other, (int, float)
-                          ), "only supporting int/float powers for now"
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
         out = Value(self.data**other, (self,), f'**{other}')
 
         def _backward():
@@ -50,52 +42,20 @@ class Value:
 
         return out
 
-    def __neg__(self):  # -self
-        return self * -1
-
-    def __radd__(self, other):  # other + self
-        return self + other
-
-    def __sub__(self, other):  # self - other
-        return self + (-other)
-
-    def __rsub__(self, other):  # other - self
-        return other + (-self)
-
-    def __rmul__(self, other):  # other * self
-        return self * other
-
-    def __truediv__(self, other):  # self / other
-        return self * other**-1
-
-    def __rtruediv__(self, other):  # other / self
-        return other * self**-1
-
-    def tanh(self):
-        x = self.data
-        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
-        out = Value(t, (self, ), 'tanh')
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
 
         def _backward():
-            self.grad += (1 - t**2) * out.grad
-        out._backward = _backward
-
-        return out
-
-    def exp(self):
-        out = Value(math.exp(self.data), (self, ), 'exp')
-
-        def _backward():
-            self.grad += out.data * out.grad
+            self.grad += (out.data > 0) * out.grad
         out._backward = _backward
 
         return out
 
     def backward(self):
+
         # topological order all of the children in the graph
         topo = []
         visited = set()
-
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
@@ -108,3 +68,27 @@ class Value:
         self.grad = 1
         for v in reversed(topo):
             v._backward()
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
